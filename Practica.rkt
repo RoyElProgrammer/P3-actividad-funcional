8d229861290d6cc8efd5de93a95671ac1510d48c
Brandon-Rivera <A01424338@itesm.mx>
2022-04-27 10:18:54 -0500

    Agregue cosas para la funcion 10

--- Practica.rkt
+++ Practica.rkt
@@ -1,11 +1,26 @@
 #lang racket
 
+#|
 (define (sign a)
   (if (= a 0) 0
       (if (< a 0) -1
       1)
       )
   )
+|#
 
-(define (pow n p)
+; Ejercicio 10: Swapper
+(define (swapper str1 str2 los)
+  (cond ((empty? los)
+         empty)
+        ((symbol=? str1 (first los))
+         (cons str2 (swapper str1 str2 (rest los))))
+        ((symbol=? str2 (first los))
+         (cons str1 (swapper str1 str2 (rest los))))
+        (else
+         (cons (first los) (swapper str1 str2 (rest los))))))
+
+;(swapper 'x 'y '(a x b c y d x y))
+
+
   
