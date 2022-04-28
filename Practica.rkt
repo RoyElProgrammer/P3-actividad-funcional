2add03015d607f56e3c0b80b9f2e0b3901af9747
RoyElProgrammer <a01424454@tec.mx>
2022-04-28 11:34:07 -0500

    BMI

--- Practica.rkt
+++ Practica.rkt
@@ -8,4 +8,19 @@
   )
 
 (define (pow n p)
-  
+  (if (= p 0)
+      1
+      (* n (pow n (- p 1)))))
+
+(define (BMI weight height)
+  (define bmi (/ weight (* height height)))
+  (if (< bmi 20)
+      "underweight"
+      (if (< bmi 25)
+          "normal"
+          (if (< bmi 30)
+              "obese1"
+              (if (< bmi 40)
+                  "obese2"
+                  "obese3"
+                  )))))
