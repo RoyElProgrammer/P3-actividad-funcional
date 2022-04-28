48305f7222ce619200425a0361a09a395bf074f0
RoyElProgrammer <a01424454@tec.mx>
2022-04-28 11:48:23 -0500

    fibonacci

--- Practica.rkt
+++ Practica.rkt
@@ -24,3 +24,13 @@
                   "obese2"
                   "obese3"
                   )))))
+
+(define i 1)
+(define (fib n)
+  (if (= n 0)
+      0
+      (if (= n 1)
+          1
+          (if (= n 2)
+              1
+              (+ (fib(- n 1)) (fib (- n 2)))))))
